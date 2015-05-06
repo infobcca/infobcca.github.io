ca08bcbc54b6a30c7184d2af901eaf9389394d42
Michele Ancheta <michelee.ancheta@gmail.com>
2015-05-06 12:09:19 -0430

    Optimizacion de codigo

--- assets/js/responsiveslides.js
+++ assets/js/responsiveslides.js
@@ -1,13 +1,3 @@
-/*! ResponsiveSlides.js v1.54
- * http://responsiveslides.com
- * http://viljamis.com
- *
- * Copyright (c) 2011-2012 @viljamis
- * Available under the MIT license
- */
-
-/*jslint browser: true, sloppy: true, vars: true, plusplus: true, indent: 2 */
-
 (function ($, window, i) {
   $.fn.responsiveSlides = function (options) {
 
