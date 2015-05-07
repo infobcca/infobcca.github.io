de6330771e39777e42869d3b754c2db46cad224d
Michele Ancheta <michelee.ancheta@gmail.com>
2015-05-07 11:54:16 -0430

    Ajustes de bandera, comentarios y reduccion de img

--- assets/js/responsiveslides.js
+++ assets/js/responsiveslides.js
@@ -11,8 +11,8 @@
       "random": false,          // Boolean: Randomize the order of the slides, true or false
       "pause": false,           // Boolean: Pause on hover, true or false
       "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
-      "prevText": "Previous",   // String: Text for the "previous" button
-      "nextText": "Next",       // String: Text for the "next" button
+      "prevText": "  Previous  ", // String: Text for the "previous" button
+      "nextText": "  Next  ",// String: Text for the "next" button
       "maxwidth": "",           // Integer: Max-width of the slideshow, in pixels
       "navContainer": "",       // Selector: Where auto generated controls should be appended to, default is after the <ul>
       "manualControls": "",     // Selector: Declare custom pager navigation
